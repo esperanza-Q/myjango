686bc15633d4bef3dc1321052f107df05c2da7f5
sujin <20241005@sungshin.ac.kr>
2025-05-10 22:07:58 +0900

    Feat:회원가입/로그인/로그아웃

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CustomUser
+from django.contrib import auth
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            
+            user = CustomUser.objects.create_user(
+                nickname = request.POST['nickname'],
+                username = request.POST['username'],
+                email = request.POST['email'],
+                password = request.POST['password1'],
+                user_image = request.FILES['user_image']
+            )
+            # 정보를 한꺼번에 담아 객체 생성 or post = Post()로 객체 먼저 생성해도 됨! 적절하게 사용.
+            
+            return render(request, 'login.html')   
+        return render(request, 'signup.html') 
+    return render(request, 'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return render(request, 'signupfin.html')
+        else:
+            return render(request, 'login.html')
+        
+    else:
+        return render(request, 'login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('accounts:login')    
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import home, toLogin, toSignup
+
+app_name='home'
 
 urlpatterns = [
     path('home/', home, name='home'),
+    path('login/', toLogin, name='toLogin'),
+    path('signup/', toSignup, name='toSignup')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
