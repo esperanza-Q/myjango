7d3fc5ac2819d3fc3cfb402e6762e4eda10ef137
esperanza-Q <164371050+esperanza-Q@users.noreply.github.com>
2025-05-20 06:43:11 +0900

    feat : scrap_home,create,delete,detail, Scrap, ScrapRecipe (#36)

--- mypage/models.py
+++ mypage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import CustomUser
+from recipe.models import Recipe
 
 # Create your models here.
 class UserIngredient(models.Model):
@@ -17,3 +18,22 @@ class UserIngredient(models.Model):
     unit = models.CharField(max_length=7, default='g', choices=UNIT_CHOICES, verbose_name="단위")
     icon = models.CharField(max_length=255, null=False, blank=False, default='img/재료아이콘/고기테스트.png')
     expiration_date = models.DateTimeField()
+
+class Scrap(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)  # 폴더 이름
+
+    def __str__(self):
+        return f"{self.user.username}의 스크랩 폴더: {self.name}"
+
+
+class ScrapRecipe(models.Model):
+    scrap = models.ForeignKey(Scrap, on_delete=models.CASCADE, related_name='scrap_recipes')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    add_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('scrap', 'recipe')  # 하나의 폴더에 중복 저장 방지
+
+    def __str__(self):
+        return f"{self.scrap.name} 폴더에 스크랩된 {self.recipe.title}"
--- mypage/urls.py
+++ mypage/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import mypage, myref, ing_add, ing_detail, ing_delete, ing_update
+from .views import mypage, myref, ing_add, ing_detail, ing_delete, ing_update, scrap, scrap_addFolder, scrap_deleteFolder, scrap_detail
 
 app_name='mypage'
 
 urlpatterns = [
     path('', mypage, name='mypage'),
+    # 마이냉장고-재료
     path('myref/', myref, name='myref'),
     path('myref/ing_add/', ing_add, name='ing_add'),
     path('myref/ing_detail/<int:ing_id>/', ing_detail, name='ing_detail'),
     path('myref/ing_detail/<int:ing_id>/delete/', ing_delete, name='ing_delete'),
     path('myref/ing_update/<int:ing_id>/', ing_update, name='ing_update'),
+    #스크랩
+    path('scrap/', scrap, name='scrap'),
+    path('scrap/scrap_add/', scrap_addFolder, name='scrap_add'),
+    path('scrap/scrap_delete/', scrap_deleteFolder, name='scrap_delete'),
+    path('scrap/scrap_detail/<int:scrap_id>/', scrap_detail, name='scrap_detail'),
 ]
