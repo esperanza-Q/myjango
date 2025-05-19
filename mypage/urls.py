f249a2b4eb96492fbc0d44ca7ba5e927b41acaf3
esperanza-Q <164371050+esperanza-Q@users.noreply.github.com>
2025-05-20 04:17:43 +0900

    feat : ingredient_update,delete (#34)

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import mypage, myref, ing_add
+from .views import mypage, myref, ing_add, ing_detail, ing_delete, ing_update
 
 app_name='mypage'
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('', mypage, name='mypage'),
     path('myref/', myref, name='myref'),
     path('myref/ing_add/', ing_add, name='ing_add'),
+    path('myref/ing_detail/<int:ing_id>/', ing_detail, name='ing_detail'),
+    path('myref/ing_detail/<int:ing_id>/delete/', ing_delete, name='ing_delete'),
+    path('myref/ing_update/<int:ing_id>/', ing_update, name='ing_update'),
 ]
--- mypage/views.py
+++ mypage/views.py
@@ -32,5 +32,50 @@ def ing_add(request):
         return redirect('mypage:myref')
         
     else:
+        ingredients = UserIngredient.objects.filter(user = request.user)
         form = IngredientForm()
-        return render(request, 'test_ing_add.html', {'form':form})
+        return render(request, 'test_ing_add.html', {'form':form, 'ingredients':ingredients})
+    
+def ing_detail(request, ing_id):
+    ing = get_object_or_404(UserIngredient, id=ing_id)
+    ingredients = UserIngredient.objects.filter(user = request.user)
+    return render(request, 'test_ing_detail.html', {'ing':ing, 'ingredients':ingredients})
+
+def ing_delete(request, ing_id):
+    ing = get_object_or_404(UserIngredient, id=ing_id)
+    ing.delete()
+    return redirect('mypage:myref')
+
+def ing_update(request, ing_id):
+    ing = get_object_or_404(UserIngredient, id=ing_id)
+    ingredients = UserIngredient.objects.filter(user = request.user)
+    if request.method == 'POST':
+        form=IngredientForm(request.POST, instance=ing)
+        
+        if request.POST['icon'] == "meat":
+            icon = 'img/재료아이콘/고기테스트.png'
+        elif request.POST['icon'] == "vegetable":
+            icon = 'img/재료아이콘/채소테스트.png'
+        elif request.POST['icon'] == "fruit":
+            icon = 'img/재료아이콘/과일테스트.png'
+        else:
+            icon = 'img/재료아이콘/소스테스트.png'
+        
+        if form.is_valid():
+            ingredient = form.save(commit=False)
+            ingredient.icon = icon
+            ingredient.user = request.user
+            ingredient.expiration_date = request.POST['expiration_date']
+            ingredient.save()
+            
+        return redirect('mypage:myref')
+    else:
+        form=IngredientForm(instance=ing)
+        context = {
+            'form':form,
+            'ingredients':ingredients,
+            'expiration_date': ing.expiration_date,
+            'icon' : ing.icon
+        }
+        print(ing.icon)
+        return render(request, 'test_ing_update.html', context)
