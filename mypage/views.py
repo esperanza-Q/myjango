956f4ab55582217f7734d969a2abe11db7f4dbec
Son <suruna1026@gmail.com>
2025-05-20 06:40:59 +0900

    feat : scrap_home,create,delete,detail, Scrap, ScrapRecipe

--- mypage/views.py
+++ mypage/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import UserIngredient
+
+from recipe.models import Recipe
+from .models import Scrap, ScrapRecipe, UserIngredient
 from .forms import IngredientForm
 
 # Create your views here.
@@ -79,3 +81,54 @@ def ing_update(request, ing_id):
         }
         print(ing.icon)
         return render(request, 'test_ing_update.html', context)
+    
+    
+#🔽스크랩 파트
+    
+def scrap(request):
+    scraps = Scrap.objects.filter(user=request.user)
+    return render(request, 'test_scrap.html', {'scraps':scraps})
+
+def scrap_addFolder(request):
+    scraps = Scrap.objects.filter(user=request.user)
+    if request.method == 'POST':
+        name=request.POST['scrapName']
+        
+        if not Scrap.objects.filter(user=request.user, name=name):
+            Scrap.objects.create(
+                user = request.user,
+                name = name
+            )
+            return redirect('mypage:scrap')
+        return render(request, 'test_scrap_add.html', {'scraps':scraps})
+    else:
+        return render(request, 'test_scrap_add.html', {'scraps':scraps})
+    
+def scrap_deleteFolder(request):
+    scraps = Scrap.objects.filter(user=request.user)
+    if request.method=='POST':
+        scrap_ids = request.POST.getlist('scrap_ids')
+        Scrap.objects.filter(id__in=scrap_ids, user=request.user).delete()
+        return redirect('mypage:scrap')
+    else:
+        return render(request, 'test_scrap_delete.html', {'scraps':scraps})
+        
+def scrap_detail(request, scrap_id):
+    scraps = Scrap.objects.filter(user=request.user)
+    scrap = get_object_or_404(Scrap, id=scrap_id)
+    scrapRecipes = ScrapRecipe.objects.filter(scrap=scrap)
+    context = {
+        'scraps':scraps,
+        'selected_scrap' : scrap,
+        'scrapRecipes' : scrapRecipes
+    }
+    return render(request, 'test_scrap_detail.html', context)
+    
+# def add_to_scrap_folder(request, recipe_id, scrap_id):
+#     scrap = Scrap.objects.get(id=scrap_id, user=request.user)
+#     recipe = Recipe.objects.get(id=recipe_id)
+
+#     # 이미 스크랩되어 있는지 확인
+#     if not ScrapRecipe.objects.filter(scrap=scrap, recipe=recipe).exists():
+#         ScrapRecipe.objects.create(scrap=scrap, recipe=recipe)
+    
