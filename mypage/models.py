245aa02ab381f5a67c06f6440b2dfd2a282bd562
Son <suruna1026@gmail.com>
2025-05-19 16:46:54 +0900

    feat : mypage,ing_add

--- mypage/models.py
+++ mypage/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+class UserIngredient(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    amounts = models.IntegerField(null=False)
+    UNIT_CHOICES = (
+        ('g', 'g'),
+        ('kg', 'kg'),
+        ('ml', 'ml'),
+        ('L', 'L'),
+        ('개', '개'),
+        ('마리', '마리')
+    )
+    unit = models.CharField(max_length=7, default='g', choices=UNIT_CHOICES, verbose_name="단위")
+    icon = models.CharField(max_length=255, null=False, blank=False, default='img/재료아이콘/고기테스트.png')
+    expiration_date = models.DateTimeField()
--- mypage/views.py
+++ mypage/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import UserIngredient
+from .forms import IngredientForm
 
 # Create your views here.
+def mypage(request):
+    return render(request, 'test_mypage.html')
+
+def myref(request):
+    ingredients = UserIngredient.objects.filter(user = request.user)
+    return render(request, 'test_myref.html', {'ingredients':ingredients})
+
+def ing_add(request):
+    if request.method == 'POST':
+        form = IngredientForm(request.POST)
+        if request.POST['icon'] == "meat":
+            icon = 'img/재료아이콘/고기테스트.png'
+        elif request.POST['icon'] == "vegetable":
+            icon = 'img/재료아이콘/채소테스트.png'
+        elif request.POST['icon'] == "fruit":
+            icon = 'img/재료아이콘/과일테스트.png'
+        else:
+            icon = 'img/재료아이콘/소스테스트.png'
+
+        if form.is_valid():
+            ingredient = form.save(commit=False)
+            ingredient.icon = icon
+            ingredient.user = request.user
+            ingredient.expiration_date = request.POST['expiration_date']
+            ingredient.save()
+        
+        return redirect('mypage:myref')
+        
+    else:
+        form = IngredientForm()
+        return render(request, 'test_ing_add.html', {'form':form})
