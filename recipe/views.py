8c3fcb8059bd8ee7b2fdcbf6440d55d8aee0cd8b
Son <suruna1026@gmail.com>
2025-05-21 14:54:18 +0900

    feat : recipeAddScrap,scrapCheckRecipe

--- recipe/views.py
+++ recipe/views.py
@@ -22,3 +22,10 @@ def create(request):
 def detail(request, recipe_id):
     recipe = Recipe.objects.filter(pk=recipe_id).first
     return render(request, 'recipe_detail.html', {'recipe':recipe})
+
+
+#여기는 테스트 용도 리스트 함수
+# 나중에 지울 거임
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'test_recipe_list.html', {'recipes': recipes})
